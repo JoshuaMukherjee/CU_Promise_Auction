5a6365d6a7878fb659ce93ff154d34633d692fec
JoshuaMukherjee <joshuaakmukherjee@gmail.com>
2024-05-18 18:01:28 +0100

    settings

--- biddingsoftware/settings.py
+++ biddingsoftware/settings.py
@@ -46,8 +46,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'import_export',
-    'lboro-promise-auction'
+    'import_export'
 ]
 
 MIDDLEWARE = [
